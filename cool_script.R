17e303383bfbb249e93c2ee6dc99878e731c2f48
Natalie Namesna <hanusova.nat@gmail.com>
2026-01-28 14:42:56 +0100

    calculations

--- cool_script.R
+++ cool_script.R
@@ -2,3 +2,14 @@
 usethis::use_github()
 
 # cauky
+
+# Create new branch and switch to it
+usethis::pr_init("my-feature")
+
+# Make changes, commit them
+a <- 11
+b <- 12
+c <- a + b
+
+# Push branch to GitHub and create PR
+usethis::pr_push()
